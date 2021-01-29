d0aa914623a784807741c4d78dfa6181811d1c6b
talryan <nataliamryan@gmail.com>
2021-01-29 10:38:02 -0800

    commit tree 092900f3972c628c78d722afe371e14589e4d441

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Your code here
-var dodger = document.getElementById('dodger');
+const dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '');
-  var left = parseInt(leftNumbers, 10)
+ let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
 }
 
 function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '');
-  var left = parseInt(leftNumbers, 10)
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
   if (left > 0) {
     dodger.style.left = `${left + 1}px`
   }
